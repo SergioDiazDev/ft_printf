1d66e31383b385a93a7e6ef59ccc7f23f7615bb0
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-02-21 17:11:02 +0100

    Implimentado %c y %s

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 10:43:24 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/02/21 13:54:25 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/02/21 17:09:32 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 int	main(void)
 {
+	char a = 'a';
+	char str[4] = "asd\0";
 	printf("\n");
-	ft_printf("hola %c.", 'a');
+	ft_printf("hol%c %s", a, str);
 	printf("\n\n");
 	//system("leaks main.out");
 }
