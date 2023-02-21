406d2504e67228d48a947c8a80a2189dd1010f72
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-02-21 15:47:35 +0100

    Creado el make e implementado el switch

--- ft_printf.c
+++ ft_printf.c
@@ -1,18 +1,56 @@
-#include <stdarg.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 10:42:52 by sdiaz-ru          #+#    #+#             */
+/*   Updated: 2023/02/21 13:53:43 by sdiaz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
-#include <stdio.h>
 
-int ft_printf(char const *str, ...)
+int	ft_printf(char const *str, ...)
 {
-    va_list ptr;
-    va_start(ptr, str);
+	int		*len;
+	int		i;
+	va_list	ptr;
 
-    printf("%s\n", str);
-    str = va_arg(ptr, char *);
-    printf("%s\n", str);
-    str = va_arg(ptr, char *);
-    printf("%s\n", str);
-    va_end(ptr);
+	va_start(ptr, str);
+	len = &i;
+	i = -1;
+	while (str[++i])
+	{
+		ft_switch(str, len, ptr);
+	}
+	va_end(ptr);
+	return (1);
+}
 
-    return (1);
+void	ft_switch(char const *str, int *len, va_list ptr)
+{
+	if (str[*len] != '%')
+		write(1, &str[*len], 1);
+	else
+	{
+		(*len)++;
+		if (str[*len] == 'c')
+			write(1, va_arg(ptr, char *), 1);
+		else if (str[*len] == 's')
+			write(1, "s", 1);
+		else if (str[*len] == 'p')
+			write(1, "p", 1);
+		else if (str[*len] == 'd')
+			write(1, "d", 1);
+		else if (str[*len] == 'i')
+			write(1, "i", 1);
+		else if (str[*len] == 'u')
+			write(1, "u", 1);
+		else if (str[*len] == 'x')
+			write(1, "x", 1);
+		else if (str[*len] == 'X')
+			write(1, "X", 1);
+	}
 }
--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 10:43:24 by sdiaz-ru          #+#    #+#             */
+/*   Updated: 2023/02/21 13:54:25 by sdiaz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdlib.h>
 
-int main(void)
+int	main(void)
 {
-    ft_printf("HOlaa", "Buenas", "tardes");
+	printf("\n");
+	ft_printf("hola %c.", 'a');
+	printf("\n\n");
+	//system("leaks main.out");
 }
