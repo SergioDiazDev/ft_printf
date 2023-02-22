ec6df97b8bddc538e4662f84799aed1b791de91e
sdiaz-ru <sdiaz-ru@studient.42malaga.com>
2023-02-22 16:25:37 +0100

    Implementados todos menos %p

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 10:43:24 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/02/22 15:43:38 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/02/22 16:23:20 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	main(void)
 {
-	ft_putbase(42, "0123456789\0");
-	// char a = 'a';
-	// char str[4] = "asd\0";
-	// int num = -500;
-	// printf("\n");
-	// ft_printf("hol%c %s %d", a, str, num);
-	// printf("\n\n");
+	//ft_putbase(42, "0123456789\0");
+	char a = 'a';
+	char str[50] = "asdsadasdasdasdasdasdasd\0";
+	long num = -500;
+	long i = 42;
+	unsigned int u = 747385742;
+	int x= 42;
+	int X= 42;
+	printf("\n");
+	ft_printf("c|%c|\ns|%s|\nd|%d|\ni|%i|\nu|%u|\nx|%x|\nX|%X|\n %%|%%|", a, str, num, i, u, x, X);
+	printf("\n\n");
 	//system("leaks main.out");
 }
